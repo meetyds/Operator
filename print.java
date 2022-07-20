0d0d807fabf7b8b76ed51f653e9fe8cef222eba2
Meet Patel <meet.yds@gmail.com>
2022-07-20 18:08:30 +0100

    Operator

---
+++ print.java
@@ -0,0 +1,14 @@
+package Concept;
+
+
+public class print {
+    public static void main(String[] args) {
+
+        System.out.println("My First Program");
+        int a = 21;
+        System.out.println(a++); // 21 a= 21 ++ means  a=21+1=22 a=22
+        System.out.println(++a);// 22 a=22 a=a+1 a=23
+        System.out.println(+a); //23
+        System.out.print(a); //23
+    }
+}
